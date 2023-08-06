3ab15d18eb546da69807039f1e2c7ca03ed628f1
ayush <ayushkhairnar06@gmail.com>
2023-08-06 21:42:23 +0530

    fdfwefwe

---
+++ loops.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int sum=0;
+    int i=0;
+    while(i!=5){
+        sum=sum+i;
+        i++;
+    }
+    cout<<"sum is "<<sum<<endl;
+    return 0;
+
+}
