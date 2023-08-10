bcf77c1401811f34467953003aa48ea20d5f2d68
ayush <ayushkhairnar06@gmail.com>
2023-08-10 22:55:41 +0530

    to calculate areav using functions

---
+++ area.cpp
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+float radius(int x){
+float area=3.14*x*x;
+return area;
+    
+}
+
+int main()
+{
+    cout<<radius(3);
+
+    return 0;
+}
