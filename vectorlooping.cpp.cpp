223ddf9fa44bf87f1ac3cd646fa39bca3918ad8e
TechNinjaAyush <141556340+TechNinjaAyush@users.noreply.github.com>
2023-08-11 23:31:22 +0530

    Add files via upload

---
+++ vectorlooping.cpp.cpp
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int>v;
+    for(int i=0;i<5;i++){
+        int element;
+        cin>>element;
+         v.push_back(element);
+    }
+    for(int i=0;i<v.size();i++){
+        cout<<v[i]<<endl;
+    }
+
+    return 0;
+}
