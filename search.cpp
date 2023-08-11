be83a0ede038e52dccdeba79c737fede7548828e
TechNinjaAyush <141556340+TechNinjaAyush@users.noreply.github.com>
2023-08-11 19:56:40 +0530

    Update search.cpp

--- search.cpp
+++ search.cpp
@@ -1,7 +1,9 @@
 #include<iostream>
 using namespace std;
 int main(){
-int number=1;
+int number=1;    //we have to find wheather 1 is repeting in arrays or not;
+                  //in key we stored -1 so if number 1 is find in an array then updation of ket occurs;
+                      //using break we end loop som that we dont need to check in all elements of array
 int key=-1;
 int arr[5]={4,3,1,2,7};
 for(int i=0;i<5;i++){
