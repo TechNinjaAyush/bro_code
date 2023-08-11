40531025ed8e37e23c1d68761b688a3a0bfe1ef6
TechNinjaAyush <141556340+TechNinjaAyush@users.noreply.github.com>
2023-08-11 19:39:33 +0530

    Add files via upload

---
+++ max.cpp
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ int arr[5]={1,2,3,5,4};
+   int  max=0;
+ for(int i=0;i<5;i++){
+     if(arr[i]>max){
+         max=arr[i];
+     }
+    
+}
+ cout<<max<<endl;
+    return 0;
+}
