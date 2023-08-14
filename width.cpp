bfaff764910855ac17c08feffae16557342ff209
TechNinjaAyush <141556340+TechNinjaAyush@users.noreply.github.com>
2023-08-14 22:52:21 +0530

    Add files via upload

---
+++ width.cpp
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+   int n,h;
+   cin>>n>>h;
+   int arr[n];
+   int sum=0;
+  for(int i=0;i<n;i++){
+      cin>>arr[i];
+      if(arr[i]<=h){
+          sum+=1;
+      }
+      else{
+          sum+=2;
+      }
+  }
+ cout<<sum; 
+    return 0;
+}
