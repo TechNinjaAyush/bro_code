39d2601b56069e36b999716f0e32c6879e1d8901
TechNinjaAyush <141556340+TechNinjaAyush@users.noreply.github.com>
2023-09-01 15:56:55 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -14,26 +14,46 @@ struct students{
    int sgpa;
 };
  class seit{
-     struct students p[15];
+     struct students p[2];
      public:
-     void bubble_sort(students p[15]){
-    cout<<"sorting roll number of students in ascending order";
-    for(int i=0;i<15;i++){
+     void enterdata(){
+     for(int i=0;i<2;i++){
+      cout<<"enter roll number \n ";
+    cin>>p[i].roll_no;
+    cout<<"enter sgpa of students \n";
+    cin>>p[i].sgpa;
+    cout<<"enter name of students \n";
+    cin>>p[i].name;
+    
+  }
+     }
+     public:
+     void bubble_sort(){
+    cout<<"sorting roll number of students in ascending order \n";
+    for(int i=0;i<2;i++){
         if(p[i].roll_no>p[i+1].roll_no){
-            swap(p[i].roll_no,p[i+1].roll_no);
+            swap(p[i],p[i+1]);
         }
     }
-    cout<<"DISPLAY ROLL NUMBER OF STUDENTS IN INCREASING ORDER";
-    for(int i=0;i<15;i++){
+    cout<<"DISPLAY ROLL NUMBER OF STUDENTS IN INCREASING ORDER \n";
+    for(int i=0;i<2;i++){
         cout<<"roll numbers of students in increasing order"<<p[i].roll_no<<" ,";
     }
 }
-void sequentialsearch(students p[15]){
-  cout<<"enter the sgpa to be searched";
+public :
+void display(){
+cout<<"NAME\t\tRoll number\t\tsgpa";
+for(int i=0;i<2;i++){
+cout<<p[i].name<<p[i].roll_number<<p[i].sgpa;
+}
+}
+public :
+void sequentialsearch(){
+  cout<<"enter the sgpa to be searched \n";
   int key;
   cin>>key;
   int flag;
-  for(int i=0;i<15;i++){
+  for(int i=0;i<2;i++){
     flag=0;
     if(key==p[i].sgpa){
    flag++;
@@ -43,25 +63,32 @@ void sequentialsearch(students p[15]){
     }
   }
 }
+public :
+void insertionsort(){
+int i,int j,int key;
+for(int i=1,i<n;i++){
 
- }
+}
+ };
 
 
 int main(){
-  students p[15];
+  students p[2];
   seit obj1;
-  cout<<"ENTER ROLL NO,sgpa,name OF 15 STUDENTS";
-  for(int i=0;i<15;i++){
-      cout<<"enter roll number ";
-    cin>>p[i].roll_no;
-    cout<<"enter sgpa of students";
-    cin>>p[i].sgpa;
-    cout<<"enter name of students";
-    cin>>p[i].name;
-    
-  }
+ 
+ obj1.enterdata(p);
+   int choice;
+  cout<<"enter the choice \n";
+  cin>>choice;
+  ("enter 1 for bubble sort/n");
+   ("enter 2 for sequential search/n");
+
+  switch(choice){
+  case 1:
    obj1.bubble_sort(p);
+   case 2:
     obj1.sequentialsearch(p);
-    return 0;
+    case 
+        
    }
-    
+      return 0;}
