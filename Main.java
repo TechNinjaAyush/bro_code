3e3e9ab5cb9644319dd0bc899c5d114cf7f88b9b
TechNinjaAyush <141556340+TechNinjaAyush@users.noreply.github.com>
2023-09-07 12:04:09 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,121 +1,67 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-import java.util.*;
+/******************************************************************************
 
-class ComplexNumber {
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
 
-    double real, image;
-
-    public ComplexNumber(double r, double i) {
-        this.real = r;
-        this.image = i;
+*******************************************************************************/
+import java.util.Scanner;
+public class employee{
+    String name;
+    int id;
+    String mail;
+    String mobile_no;
+     String address;
+    public  void getdata(){
+         Scanner sc=new Scanner(System.in);
+         System.out.println("enter the name of employee:");
+         name=sc.nextLine();
+         System.out.println("enter the ID of employee :");
+         id=sc.nextInt();
+         System.out.println("enter the email of employee:");
+         mail=sc.nextLine();
+         System.out.println("enter the mobile_number of employee:");
+         mobile_no=sc.nextLine();
+         System.out.println("enter the address of employee:");
+         address=sc.nextLine();
+     }
+     public void displaydata(){
+         System.out.println("name of employee"+name+"id is"+mail+"mobile_no"+mobile_no+"address is"+address);
+     }
+     void salarySlip(int BasePay){
+        System.out.println("\n\nDA: " + (0.95*BasePay) + "\nHRA: " + (0.1*BasePay) + "\nPF: " + (0.12*BasePay) + "\nStaff Club Fund: " + (0.001*BasePay));
+        System.out.println("Gross Salary: " + (BasePay * 2.191));
+        System.out.println("Net Salary: " + (BasePay * 2.07));
     }
-
-    public void showC() {
-        if (this.image<0){
-            System.out.print(this.real +" "+  this.image+"i");
+    public class programmer extend employee{
+        @Override
+        int BasePay
+        public void getdata(){
+        super.getdata();
         }
-        else{
-            System.out.println(this.real + "+" + this.image + "i");
+        @Override
+        public void displaydata(){
+            super.displaydata();
         }
-    }
+        @Override
+        public void salarySlip(int bp){
+            super.salarySlip(bp);
+        }
 
-    public static ComplexNumber add(ComplexNumber n1, ComplexNumber n2) {
-        ComplexNumber res = new ComplexNumber(0, 0);
-        res.real = n1.real + n2.real;
-        res.image = n1.image + n2.image;
-        return res;
-    }
-
-    public static ComplexNumber sub(ComplexNumber n1, ComplexNumber n2) {
-        ComplexNumber abc = new ComplexNumber(0, 0);
-        abc.real = n1.real - n2.real;
-        abc.image = n1.image - n2.image;
-        return abc;
-    }
-
-    public static  ComplexNumber multiply(ComplexNumber n1, ComplexNumber n2) {
-        ComplexNumber mul = new ComplexNumber(0, 0);
-        mul.real = n1.real * n2.real - n1.image * n2.image;
-        mul.image = n1.image * n2.real + n2.image * n1.real;
-        return mul;
-    }
-
-    public static ComplexNumber divide(ComplexNumber n1, ComplexNumber n2) {
-        ComplexNumber div = new ComplexNumber(0, 0);
-        double denominator = n2.real * n2.real + n2.image * n2.image;
-        div.real = (n1.real * n2.real + n1.image * n2.image) / denominator;
-        div.image = (n1.image * n2.real - n1.real * n2.image) / denominator;
-        return div;
+    
     }
 }
+ 
 
-class Main {
-    public static void main(String[] args) {
-        double a, b, c, d;
-        int choice;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter real part of first number:");
-        a = sc.nextDouble();
-        System.out.println("Enter imaginary part of first number:");
-        b = sc.nextDouble();
-        System.out.println("Enter real part of second number:");
-        c = sc.nextDouble();
-        System.out.println("Enter real part of second number:");
-        d = sc.nextDouble();
+public class Main
+{
+	public static void main(String[] args) {
+int basepay=10000;
+   programmer P1=new programmer();
+   p1.getdata();
+   p1.displaydata();
+   p1.salarySlip()
 
-        // Creating two complex numbers
-        ComplexNumber c1 = new ComplexNumber(a, b);
-        ComplexNumber c2 = new ComplexNumber(c, d);
-
-        System.out.print("First Complex number: ");
-        c1.showC();
-        System.out.print("\nSecond Complex number: ");
-        c2.showC();
-
-        System.out.println("\n================================================================");
-
-        do {
-            System.out.println("\nChoose the operation you want to perform:");
-            System.out.println("1.Addition\n2.Subtraction\n3.Multiplication\n4.Division\n5.Exit");
-            choice = sc.nextInt();
-            switch (choice) {
-                case 1:
-                    ComplexNumber res = ComplexNumber.add(c1, c2);
-                    System.out.println("\nAddition is:");
-                    res.showC();
-                    break;
-
-                case 2:
-                    ComplexNumber abc = ComplexNumber.sub(c1, c2);
-                    System.out.println("\nSubtraction is:");
-                    abc.showC();
-                    break;
-
-                case 3:
-                    ComplexNumber mul = ComplexNumber.multiply(c1, c2);
-                    System.out.println("\nMultiplication is:");
-                    mul.showC();
-                    break;
-
-                case 4:
-                    ComplexNumber div = ComplexNumber.divide(c1, c2);
-                    System.out.println("\nDivision is:");
-                    div.showC();
-                    break;
-
-                case 5:
-                    System.out.println("Exiting the program.");
-                    return;
-
-                default:
-                    System.out.println("Please enter a valid choice.");
-                    break;
-            }
-
-
-        } while (true);
-    }
-}}
+	}
+}
