9643304c1c329babe7150154f92fb7ad72efec5c
Ayush khairnar <141556340+TechNinjaAyush@users.noreply.github.com>
2023-11-21 22:28:58 +0530

    Add files via upload

---
+++ equalarrays.cpp
@@ -0,0 +1,68 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+//{ Driver Code Starts
+//Initial function template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long 
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+    public:
+
+    //Function to check if two arrays are equal or not.
+    bool check(vector<ll> A, vector<ll> B, int N) {
+     sort ( A.begin() ,  A.end()) ;
+     sort ( B.begin() , B.end()) ;
+      vector< pair <long long,long long > > vect;
+      for ( int i = 0 ; i< N ;i++){
+          vect.push_back(make_pair(A[i] , B[i])) ;
+          
+      }
+      
+      for (auto it : vect){
+          if(it.first != it.second){
+              return false ;
+          }
+      }
+      return true ;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+ {
+    int t;
+    cin>>t;
+    while(t--) {
+        int n;
+        cin>>n;
+        
+        vector<ll> arr(n,0),brr(n,0);
+        
+        // increase the count of elements in first array
+        for(ll i=0;i<n;i++)
+            cin >> arr[i];
+        
+        
+        // iterate through another array
+        // and decrement the count of elements
+        // in the map in which frequency of elements
+        // is stored for first array
+        for(ll i=0;i<n;i++)
+            cin >> brr[i];
+        Solution ob;
+        cout << ob.check(arr,brr,n) << "\n";
+    }
+	return 0;
+}
+// } Driver Code Ends
