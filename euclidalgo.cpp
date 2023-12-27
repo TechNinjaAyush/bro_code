f652cc7181999e57d03b9f1ace3c0e85c564a996
Ayush khairnar <141556340+TechNinjaAyush@users.noreply.github.com>
2023-12-28 00:00:58 +0530

    Add files via upload

---
+++ euclidalgo.cpp
@@ -0,0 +1,30 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{  
+  
+  c = max ( n ,m) ;
+  d  = min ( n,m) ;
+  while ( d!=0) {
+      
+      int quotient = c / d ;
+      int rem = c % d ;
+      c = d ;
+      d = rem ;
+      
+  }
+  cout << c ;
+   
+   
+    return 0;
+}
